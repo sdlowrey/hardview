529c87e9f5899b1241cf8e9f0f7eaed3217325e1
Scott Lowrey <sdlowrey@gmail.com>
2013-01-24 10:07:29 -0500

    Basic Entry Point validation and parsing

--- src/SmBios.h
+++ src/SmBios.h
@@ -7,6 +7,32 @@ typedef unsigned short u16;	// word
 typedef unsigned int u32;	// dword
 typedef unsigned long u64;
 
+
+// This workaround is borrowed from dmidecode types.h.
+// BIGENDIAN macro variants removed; add later if any platforms need it
+#ifdef __ia64__
+#define ALIGNMENT_WORKAROUND
+#endif
+
+#ifdef ALIGNMENT_WORKAROUND
+static inline u64 U64(u32 low, u32 high)
+{
+	u64 self;
+
+	self.l = low;
+	self.h = high;
+
+	return self;
+}
+#	define WORD(x) (u16)((x)[0] + ((x)[1] << 8))
+#	define DWORD(x) (u32)((x)[0] + ((x)[1] << 8) + ((x)[2] << 16) + ((x)[3] << 24))
+#	define QWORD(x) (U64(DWORD(x), DWORD(x + 4)))
+#else /* ALIGNMENT_WORKAROUND */
+#	define WORD(x) (u16)(*(const u16 *)(x))
+#	define DWORD(x) (u32)(*(const u32 *)(x))
+#	define QWORD(x) (*(const u64 *)(x))
+#endif /* ALIGNMENT_WORKAROUND */
+
 /**
  *  @class SmBios SmBios.h
  *  @brief Parse System Management BIOS (SMBIOS) structures.
@@ -20,9 +46,11 @@ private:
 	void log(std::string);
 	u8 *getNonEfiEntryPoint();
 	u8 *getEfiEntryPoint() {};
-	void parseEntryPoint(u8 *ep);
+	bool parseEntryPoint(u8 *ep);
 
 	const std::string path;
 	u8 majorVer;
 	u8 minorVer;
+	u16 tableLen;
+	u32 tablePtr;
 };
